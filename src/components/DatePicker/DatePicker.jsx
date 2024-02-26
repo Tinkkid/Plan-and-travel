80c5756a0b56bdb2c7ccb7b967fb5cc54e5da9ed
Yuliia Kostovynska <y.kostovynska@gmail.com>
2024-02-26 23:52:12 +0300

    Fixed styling in date picker

--- src/components/DatePicker/DatePicker.jsx
+++ src/components/DatePicker/DatePicker.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react'
-import style from './DatePicker.module.css'
+import React, { useEffect, useState } from 'react';
+import style from './DatePicker.module.css';
 import { monthNames } from '../../constans/calendar';
 import { getNumbersOfDaysInMoth, getSortedDays, range } from '../../utils';
 
@@ -7,11 +7,11 @@ const DatePicker = ({ selectedDate, handleDateChange }) => {
   const [currentMonth, setCurrentMonth] = useState(new Date().getMonth());
   const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
 
-    useEffect(() => {
-      setCurrentMonth(new Date().getMonth());
-      setCurrentYear(new Date().getFullYear());
-    }, [selectedDate]);
-
+console.log('TCL: DatePicker -> selectedDate', selectedDate);
+  useEffect(() => {
+    setCurrentMonth(new Date().getMonth());
+    setCurrentYear(new Date().getFullYear());
+  }, [selectedDate]);
 
   const nextMonth = () => {
     if (currentMonth < 11) {
@@ -22,29 +22,65 @@ const DatePicker = ({ selectedDate, handleDateChange }) => {
     }
   };
 
+  const prevMonth = () => {
+    if (currentMonth > 0) {
+      setCurrentMonth(prev => prev - 1);
+    } else {
+      setCurrentMonth(11);
+      setCurrentYear(prev => prev - 1);
+    }
+  };
+
   const handleSelectionDate = day => {
     const selected = new Date(currentYear, currentMonth, day);
-    handleDateChange(selected);
+    if (!isDateDisabled(selected)) {
+      handleDateChange(selected);
+    }
   };
 
   const isDateDisabled = date => {
     const currentDate = new Date();
+    const today = new Date(
+      currentDate.getFullYear(),
+      currentDate.getMonth(),
+      currentDate.getDate()
+    );
     const lastSelectableDate = new Date(
       currentDate.getFullYear(),
       currentDate.getMonth(),
       currentDate.getDate() + 15
     );
-    return date < currentDate || date > lastSelectableDate;
+
+    if (date.getTime() === today.getTime()) {
+      return false;
+    } else {
+      return date < today || date > lastSelectableDate;
+    }
   };
 
+  const isFirstMonth =
+    currentYear === new Date().getFullYear() &&
+    currentMonth === new Date().getMonth();
+  const isLastMonth =
+    (currentYear === new Date().getFullYear() + 1 && currentMonth === 11) ||
+    (currentYear === new Date().getFullYear() &&
+      currentMonth === new Date().getMonth() + 1);
 
   return (
     <div className={style.calendar}>
       <div className={style.calendarHeader}>
+        {!isFirstMonth && (
+          <ion-icon name="chevron-back-outline" onClick={prevMonth}></ion-icon>
+        )}
         <p>
           {monthNames[currentMonth]} {currentYear}
         </p>
-        <ion-icon name="chevron-forward-outline" onClick={nextMonth}></ion-icon>
+        {!isLastMonth && (
+          <ion-icon
+            name="chevron-forward-outline"
+            onClick={nextMonth}
+          ></ion-icon>
+        )}
       </div>
       <div className={style.calendarBody}>
         <div className={style.calendarDays}>
@@ -55,21 +91,24 @@ const DatePicker = ({ selectedDate, handleDateChange }) => {
         <div className={style.calendarDays}>
           {range(1, getNumbersOfDaysInMoth(currentYear, currentMonth) + 1).map(
             day => (
-              <p
+              <buttom
                 key={day}
                 className={`${
                   isDateDisabled(new Date(currentYear, currentMonth, day))
                     ? `${style.disabled} ${style.calendarDate}`
                     : style.calendarDate
                 } ${
-                  selectedDate && selectedDate.getDate() === day
+                  selectedDate &&
+                  selectedDate.getDate() === day &&
+                  selectedDate.getMonth() === currentMonth &&
+                  selectedDate.getFullYear() === currentYear
                     ? `${style.selected} ${style.calendarDate}`
                     : style.calendarDate
                 }`}
                 onClick={() => handleSelectionDate(day)}
               >
                 {day}
-              </p>
+              </buttom>
             )
           )}
         </div>
@@ -78,4 +117,4 @@ const DatePicker = ({ selectedDate, handleDateChange }) => {
   );
 };
 
-export default DatePicker
+export default DatePicker;
--- src/components/FormTrip/FormTrip.jsx
+++ src/components/FormTrip/FormTrip.jsx
@@ -1,106 +1,103 @@
-import React, { useState } from 'react'
-import style from './FormTrip.module.css'
+import React, { useState } from 'react';
+import style from './FormTrip.module.css';
 import DatePicker from '../DatePicker/DatePicker';
 
 const FormTrip = () => {
   const [startDate, setStartDate] = useState(null);
-  console.log("TCL: FormTrip -> startDate", startDate)
   const [endDate, setEndDate] = useState(null);
-  console.log("TCL: FormTrip -> endDate", endDate)
   const [showStartCalendar, setShowStartCalendar] = useState(false);
   const [showEndCalendar, setShowEndCalendar] = useState(false);
 
-    const handleStartDateChange = date => {
-      setStartDate(date);
-      setShowStartCalendar(false);
-    };
+  const handleStartDateChange = date => {
+    setStartDate(date);
+    setShowStartCalendar(false);
+  };
 
-    const handleEndDateChange = date => {
-      setEndDate(date);
-      setShowEndCalendar(false);
-    };
+  const handleEndDateChange = date => {
+    setEndDate(date);
+    setShowEndCalendar(false);
+  };
 
-    return (
-      <div>
-        <form>
-          <div className={style.container}>
-            <div className={style.inputWrapper}>
-              <label className={style.labelTitle}>
-                <span className={style.starRequired}>*</span>City
-              </label>
-              <input
-                className={style.inputContent}
-                readOnly
-                placeholder="Please select a city"
-              />
-            </div>
-            <div className={style.inputWrapper}>
-              <label className={style.labelTitle}>
-                <span className={style.starRequired}>*</span>Start date
-              </label>
-              <div className={style.datePickerWrapper}>
-                <button
-                  className={`${
-                    startDate
-                      ? style.btnContent
-                      : `${style.btnContent} ${style.btnContentPlaceholder}`
-                  }`}
-                  onClick={e => {
-                    e.preventDefault();
-                    setShowStartCalendar(true);
-                  }}
-                >
-                  {startDate
-                    ? `${('0' + startDate.getDate()).slice(-2)}.${(
-                        '0' +
-                        (startDate.getMonth() + 1)
-                      ).slice(-2)}.${startDate.getFullYear()}`
-                    : 'Select a date'}
-                </button>
-                {showStartCalendar && (
-                  <DatePicker
-                    selectedDate={startDate}
-                    handleDateChange={handleStartDateChange}
-                  />
-                )}
-              </div>
-            </div>
-            <div className={style.inputWrapper}>
-              <label className={style.labelTitle}>
-                <span className={style.starRequired}>*</span>End date
-              </label>
-              <div className={style.datePickerWrapper}>
-                <button
-                  className={`${
-                    startDate
-                      ? style.btnContent
-                      : `${style.btnContent} ${style.btnContentPlaceholder}`
-                  }`}
-                  onClick={e => {
-                    e.preventDefault();
-                    setShowEndCalendar(true);
-                  }}
-                >
-                  {endDate
-                    ? `${('0' + endDate.getDate()).slice(-2)}.${(
-                        '0' +
-                        (endDate.getMonth() + 1)
-                      ).slice(-2)}.${endDate.getFullYear()}`
-                    : 'Select a date'}
-                </button>
-                {showEndCalendar && (
-                  <DatePicker
-                    selectedDate={endDate}
-                    handleDateChange={handleEndDateChange}
-                  />
-                )}
-              </div>
+  return (
+    <div>
+      <form>
+        <div className={style.container}>
+          <div className={style.inputWrapper}>
+            <label className={style.labelTitle}>
+              <span className={style.starRequired}>*</span>City
+            </label>
+            <input
+              className={style.inputContent}
+              readOnly
+              placeholder="Please select a city"
+            />
+          </div>
+          <div className={style.inputWrapper}>
+            <label className={style.labelTitle}>
+              <span className={style.starRequired}>*</span>Start date
+            </label>
+            <div className={style.datePickerWrapper}>
+              <button
+                className={`${
+                  startDate
+                    ? style.btnContent
+                    : `${style.btnContent} ${style.btnContentPlaceholder}`
+                }`}
+                onClick={e => {
+                  e.preventDefault();
+                  setShowStartCalendar(true);
+                }}
+              >
+                {startDate
+                  ? `${('0' + startDate.getDate()).slice(-2)}.${(
+                      '0' +
+                      (startDate.getMonth() + 1)
+                    ).slice(-2)}.${startDate.getFullYear()}`
+                  : 'Select a date'}
+              </button>
+              {showStartCalendar && (
+                <DatePicker
+                  selectedDate={startDate}
+                  handleDateChange={handleStartDateChange}
+                />
+              )}
             </div>
           </div>
-        </form>
-      </div>
-    );
-
+          <div className={style.inputWrapper}>
+            <label className={style.labelTitle}>
+              <span className={style.starRequired}>*</span>End date
+            </label>
+            <div className={style.datePickerWrapper}>
+              <button
+                className={`${
+                  startDate
+                    ? style.btnContent
+                    : `${style.btnContent} ${style.btnContentPlaceholder}`
+                }`}
+                onClick={e => {
+                  e.preventDefault();
+                  setShowEndCalendar(true);
+                }}
+              >
+                {endDate
+                  ? `${('0' + endDate.getDate()).slice(-2)}.${(
+                      '0' +
+                      (endDate.getMonth() + 1)
+                    ).slice(-2)}.${endDate.getFullYear()}`
+                  : 'Select a date'}
+              </button>
+              {showEndCalendar && (
+                <DatePicker
+                  selectedDate={endDate}
+                  handleDateChange={handleEndDateChange}
+                />
+              )}
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 };
 
-export default FormTrip
+export default FormTrip;
