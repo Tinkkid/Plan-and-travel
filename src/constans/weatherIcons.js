84f52fac69b4b4dd342efada3d8415f54787544f
Yuliia Kostovynska <y.kostovynska@gmail.com>
2024-02-29 19:44:02 +0300

    Fixed path to svg

--- src/constans/weatherIcons.js
+++ src/constans/weatherIcons.js
@@ -1,28 +1,55 @@
+// const weatherIcons = {
+//   cloudy: '../../assets/WeatherIcons/cloudy.svg',
+//   fog: '../../assets/WeatherIcons/fog.svg',
+//   'partly-cloudy-day': '../../assets/WeatherIcons/partly-cloudy-day.svg',
+//   'clear-day': '../../assets/WeatherIcons/clear-day.svg',
+//   'clear-night': '../../assets/WeatherIcons/clear-night.svg',
+//   hail: '../../assets/WeatherIcons/hail.svg',
+//   'partly-cloudy-night': '../../assets/WeatherIcons/partly-cloudy-night.svg',
+//   'rain-snow-showers-day':
+//     '../../assets/WeatherIcons/rain-snow-showers-day.svg',
+//   'rain-snow-showers-night':
+//     '../../assets/WeatherIcons/rain-snow-showers-night.svg',
+//   'rain-snow': '../../assets/WeatherIcons/rain-snow.svg',
+//   rain: '../../assets/WeatherIcons/rain.svg',
+//   'showers-day': '../../assets/WeatherIcons/showers-night.svg',
+//   sleet: '../../assets/WeatherIcons/sleet.svg',
+//   'snow-showers-day': '../../assets/WeatherIcons/snow-showers-day.svg',
+//   'snow-showers-night': '../../assets/WeatherIcons/snow-showers-night.svg',
+//   snow: '../../assets/WeatherIcons/snow.svg',
+//   'thunder-rain': '../../assets/WeatherIcons/thunder-rain.svg',
+//   'thunder-showers-day': '../../assets/WeatherIcons/thunder-showers-day.svg',
+//   'thunder-showers-night':
+//     '../../assets/WeatherIcons/thunder-showers-night.svg',
+//   thunder: '../../assets/WeatherIcons/thunder.svg',
+//   wind: '../../assets/WeatherIcons/wind.svg',
+// };
+
+// export default weatherIcons;
+
 const weatherIcons = {
-  cloudy: '../../assets/WeatherIcons/cloudy.svg',
-  fog: '../../assets/WeatherIcons/fog.svg',
-  'partly-cloudy-day': '../../assets/WeatherIcons/partly-cloudy-day.svg',
-  'clear-day': '../../assets/WeatherIcons/clear-day.svg',
-  'clear-night': '../../assets/WeatherIcons/clear-night.svg',
-  hail: '../../assets/WeatherIcons/hail.svg',
-  'partly-cloudy-night': '../../assets/WeatherIcons/partly-cloudy-night.svg',
-  'rain-snow-showers-day':
-    '../../assets/WeatherIcons/rain-snow-showers-day.svg',
-  'rain-snow-showers-night':
-    '../../assets/WeatherIcons/rain-snow-showers-night.svg',
-  'rain-snow': '../../assets/WeatherIcons/rain-snow.svg',
-  rain: '../../assets/WeatherIcons/rain.svg',
-  'showers-day': '../../assets/WeatherIcons/showers-night.svg',
-  sleet: '../../assets/WeatherIcons/sleet.svg',
-  'snow-showers-day': '../../assets/WeatherIcons/snow-showers-day.svg',
-  'snow-showers-night': '../../assets/WeatherIcons/snow-showers-night.svg',
-  snow: '../../assets/WeatherIcons/snow.svg',
-  'thunder-rain': '../../assets/WeatherIcons/thunder-rain.svg',
-  'thunder-showers-day': '../../assets/WeatherIcons/thunder-showers-day.svg',
-  'thunder-showers-night':
-    '../../assets/WeatherIcons/thunder-showers-night.svg',
-  thunder: '../../assets/WeatherIcons/thunder.svg',
-  wind: '../../assets/WeatherIcons/wind.svg',
+  cloudy: '/assets/WeatherIcons/cloudy.svg',
+  fog: '/assets/WeatherIcons/fog.svg',
+  'partly-cloudy-day': '/assets/WeatherIcons/partly-cloudy-day.svg',
+  'clear-day': '/assets/WeatherIcons/clear-day.svg',
+  'clear-night': '/assets/WeatherIcons/clear-night.svg',
+  hail: '/assets/WeatherIcons/hail.svg',
+  'partly-cloudy-night': '/assets/WeatherIcons/partly-cloudy-night.svg',
+  'rain-snow-showers-day': '/assets/WeatherIcons/rain-snow-showers-day.svg',
+  'rain-snow-showers-night': '/assets/WeatherIcons/rain-snow-showers-night.svg',
+  'rain-snow': '/assets/WeatherIcons/rain-snow.svg',
+  rain: '/assets/WeatherIcons/rain.svg',
+  'showers-day': '/assets/WeatherIcons/showers-night.svg',
+  sleet: '/assets/WeatherIcons/sleet.svg',
+  'snow-showers-day': '/assets/WeatherIcons/snow-showers-day.svg',
+  'snow-showers-night': '/assets/WeatherIcons/snow-showers-night.svg',
+  snow: '/assets/WeatherIcons/snow.svg',
+  'thunder-rain': '/assets/WeatherIcons/thunder-rain.svg',
+  'thunder-showers-day': '/assets/WeatherIcons/thunder-showers-day.svg',
+  'thunder-showers-night': '/assets/WeatherIcons/thunder-showers-night.svg',
+  thunder: '/assets/WeatherIcons/thunder.svg',
+  wind: '/assets/WeatherIcons/wind.svg',
 };
 
 export default weatherIcons;
+
